981c9fbc0e573d556a90993c4c9300aa8106bb27
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:50:03 +0300

    dfgh

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
-
+    return render_template("home.html")
+@app.route('/colors')
+def colors():
+    return render_template("colors.html")
 if __name__ == '__main__':
    app.run(debug = True)
+
+@app.route('/')
+def home_page():
+    return render_template("colors.html")
+   	
